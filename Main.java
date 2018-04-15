fd56b2633e1a13f6871c6e7d774b236d1cb4787c
Vikram Cothur <vcothur7@gmail.com>
2018-04-15 22:46:32 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+import package1.file1;
+import package2.file2;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        file1 f1 = new file1();
+        file2 f2 = new file2();
+        f1.getname();
+        f2.getname();
+    }
+}
